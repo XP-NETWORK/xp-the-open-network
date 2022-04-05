1201fb6905ce1e56dd9f4c2946405253bec481a9
rocalex <rocmaildev@gmail.com>
2022-04-05 05:16:24 +0300

    add unit tests using chai

--- src/example.ts
+++ src/example.ts
@@ -3,7 +3,7 @@ import TonWeb from "tonweb";
 
 dotenv.config()
 
-const tonWeb = new TonWeb(new TonWeb.HttpProvider(process.env.TONCENTER_RPC_URL));
+const tonWeb = new TonWeb(new TonWeb.HttpProvider(process.env.TONCENTER_RPC_URL, { apiKey: process.env.TONCENTER_API_KEY }));
 const Address = TonWeb.Address;
 
 (async () => {
