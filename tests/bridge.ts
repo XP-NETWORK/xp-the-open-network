285a72834b5bb7ba3bef058ba1c9cd9179ff8a19
rocalex <rocmaildev@gmail.com>
2022-04-05 10:13:40 +0300

    initial unit test

--- tests/bridge.ts
+++ tests/bridge.ts
@@ -1,7 +1,33 @@
 import { expect } from 'chai'
+import * as dotenv from 'dotenv'
+import TonWeb from "tonweb";
 
-describe('Example tests', () => {
-    it('test case 1', () => {
-        console.log("Hello world!");
+dotenv.config()
+
+const tonWeb = new TonWeb(new TonWeb.HttpProvider(process.env.TONCENTER_RPC_URL));
+
+describe('Bridge', () => {
+    it('validate nft', () => {
+        // TODO: 
+    });
+
+    it('withdraw nft', () => {
+        // TODO: 
+    });
+
+    it('transfer nft to foreign', () => {
+        // TODO: 
+    });
+
+    it('transfer nft to foreign', () => {
+        // TODO: 
+    });
+
+    it('unfreeze nft from foreign', () => {
+        // TODO: 
+    });
+
+    it('validate whitelist nft', () => {
+        // TODO: 
     });
 });
