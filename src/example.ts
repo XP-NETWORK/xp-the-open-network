d64200a1939030cf0a41f322c9423a47020fd311
rocalex <rocmaildev@gmail.com>
2022-04-06 06:18:40 +0300

    deploy wallet smart contract

--- src/example.ts
+++ src/example.ts
@@ -1,12 +1,21 @@
 import * as dotenv from 'dotenv'
 import TonWeb from "tonweb";
 
+const Address = TonWeb.Address;
+const Contract = TonWeb.Contract;
+
 dotenv.config()
 
-const tonWeb = new TonWeb(new TonWeb.HttpProvider(process.env.TONCENTER_RPC_URL, { apiKey: process.env.TONCENTER_API_KEY }));
-const Address = TonWeb.Address;
+const provider = new TonWeb.HttpProvider(process.env.TONCENTER_RPC_URL, { apiKey: process.env.TONCENTER_API_KEY })
+const tonWeb = new TonWeb(provider);
 
 (async () => {
     const balance = await tonWeb.getBalance(new Address("EQALnbCowLoBEVmvSGGYKOIYySPbLKeUDTWWsl0AmqqzApek"))
     console.log(balance)
+
+    let wallet = tonWeb.wallet.create({ address: 'kQDGZirHejoWz-2s95Ln0AekT9-l3dyhJ6aYGaP9SiHg0TCh' })
+    const address = await wallet.getAddress()
+    
+    const seqno = await wallet.methods.seqno().call()
+    console.log(seqno)
 })();
